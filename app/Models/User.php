bc98f0ec1d82f2f8203c753a0a7c4b261ff3f3bf
ChenYD <wlrcyd@163.com>
2019-07-10 11:31:32 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,12 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
     protected $fillable = [
         'name', 'email', 'password',
     ];
