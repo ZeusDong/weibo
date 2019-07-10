3fe5243aeb19fed0e3a7f1680f6262ea31141100
ChenYD <wlrcyd@163.com>
2019-07-10 13:28:20 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,10 +10,15 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
     public function create()
     {
         return view('users.create');
