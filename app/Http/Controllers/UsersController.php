4e72c6b85b47c480b9f6a5483c94ab02cd4f723a
ChenYD <wlrcyd@163.com>
2019-07-10 11:39:33 +0800

    错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,6 +16,7 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
     public function store(Request $request)
     {
         $this->validate($request, [
