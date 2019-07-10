b7c3a7fa84bfdc597056ff2270c63744c15efcab
ChenYD <wlrcyd@163.com>
2019-07-10 13:21:54 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,13 @@ use App\Models\User;
 use Auth;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -38,11 +45,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
-
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
+
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
@@ -57,6 +66,6 @@ class UsersController extends Controller
 
         session()->flash('success', '个人资料更新成功！');
 
-        return redirect()->route('users.show', $user);
+        return redirect()->route('users.show', $user->id);
     }
 }
