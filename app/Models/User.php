8ef1f061e4d0686e0d91d6d7b89d690202857876
ChenYD <wlrcyd@163.com>
2019-07-10 17:41:40 +0800

    社交统计信息、

--- app/Models/User.php
+++ app/Models/User.php
@@ -92,7 +92,7 @@ class User extends Authenticatable
         }
         $this->followings()->detach($user_ids);
     }
-    
+
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
